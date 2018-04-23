d27865ae42665a4cb5cc0e40ab838c2a3510dd2e
SantiagoCastellano <santiagocastellano01@gmail.com>
2018-04-23 20:04:06 -0300

    Agrega proyecto de TEST

---
+++ Juego.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Juego.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("Juego.Test")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("136ec988-2bfe-4e35-b52e-8992d83521a9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Juego.Test/TestPartida.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Juego.Entidades;
+
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Juego.Test
+{
+    [TestClass]
+    public class TestPartida
+    {
+        [TestMethod]
+        public void DeberiaMezclarMazo()
+        {
+            
+            
+
+
+        }
+    }
+}
