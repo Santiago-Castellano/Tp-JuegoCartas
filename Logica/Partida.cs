f5a6f397262f166f9ea916fa760bddbcd88768e5
SantiagoCastellano <santiagocastellano01@gmail.com>
2018-04-29 00:33:16 -0300

    Agrega dos métodos a la clase Partida

--- Logica/Partida.cs
+++ Logica/Partida.cs
@@ -21,6 +21,104 @@ namespace Juego.Entidades
             this.JugadorUno = jugador;
         }
 
+        public void ActualizaMazosNormal(int ganador)
+        {
+            if (ganador == 1)
+            {
+                var aux = this.JugadorUno.Cartas.First();
+                var aux2 = this.JugadorDos.Cartas.First();
+                this.JugadorUno.Cartas.RemoveAt(0);
+                this.JugadorUno.Cartas.Add(aux);
+                this.JugadorDos.Cartas.RemoveAt(0);
+                this.JugadorUno.Cartas.Add(aux2);
+
+            }
+            if (ganador == 2)
+            {
+                var aux = this.JugadorDos.Cartas.First();
+                this.JugadorDos.Cartas.RemoveAt(0);
+                this.JugadorDos.Cartas.Add(aux);
+                aux = this.JugadorUno.Cartas.First();
+                this.JugadorUno.Cartas.RemoveAt(0);
+                this.JugadorDos.Cartas.Add(aux);
+            }
+        }
+
+        public void ActualizarMazoEspecial()
+        {
+            var CartaUno = this.JugadorUno.Cartas.First();
+            var CartaDos = this.JugadorDos.Cartas.First();
+            if (CartaDos.Tipo != Carta.TipoCarta.Especial & CartaUno.Tipo != Carta.TipoCarta.Especial)
+            {
+                if (CartaUno.Tipo == Carta.TipoCarta.Roja)
+                {
+                    if (CartaDos.Tipo == Carta.TipoCarta.Amarilla)
+                    {
+                        this.JugadorUno.Cartas.RemoveAt(0);
+                        this.JugadorDos.Cartas.RemoveAt(0);
+                        this.JugadorUno.Cartas.Add(CartaUno);
+                        this.JugadorUno.Cartas.Add(CartaDos);
+                        var aux = this.JugadorDos.Cartas.First();
+                        this.JugadorDos.Cartas.RemoveAt(0);
+                        this.JugadorUno.Cartas.Add(aux);
+                    }
+                    if (CartaDos.Tipo == Carta.TipoCarta.Normal)
+                    {
+                        this.JugadorUno.Cartas.RemoveAt(0);
+                        this.JugadorDos.Cartas.RemoveAt(0);
+                        this.JugadorUno.Cartas.Add(CartaUno);
+                        var aux = this.JugadorDos.Cartas.First();
+                        this.JugadorDos.Cartas.RemoveAt(0);
+                        this.JugadorUno.Cartas.Add(aux);
+                        aux = this.JugadorDos.Cartas.First();
+                        this.JugadorDos.Cartas.RemoveAt(0);
+                        this.JugadorUno.Cartas.Add(aux);
+                    }
+                }
+
+                if (CartaDos.Tipo == Carta.TipoCarta.Roja)
+                {
+                    if (CartaUno.Tipo == Carta.TipoCarta.Amarilla)
+                    {
+                        this.JugadorDos.Cartas.RemoveAt(0);
+                        this.JugadorUno.Cartas.RemoveAt(0);
+                        this.JugadorDos.Cartas.Add(CartaDos);
+                        this.JugadorDos.Cartas.Add(CartaUno);
+                        var aux = this.JugadorUno.Cartas.First();
+                        this.JugadorUno.Cartas.RemoveAt(0);
+                        this.JugadorDos.Cartas.Add(aux);
+                    }
+                    if (CartaUno.Tipo == Carta.TipoCarta.Normal)
+                    {
+                        this.JugadorDos.Cartas.RemoveAt(0);
+                        this.JugadorUno.Cartas.RemoveAt(0);
+                        this.JugadorDos.Cartas.Add(CartaDos);
+                        var aux = this.JugadorUno.Cartas.First();
+                        this.JugadorUno.Cartas.RemoveAt(0);
+                        this.JugadorDos.Cartas.Add(aux);
+                        aux = this.JugadorUno.Cartas.First();
+                        this.JugadorUno.Cartas.RemoveAt(0);
+                        this.JugadorDos.Cartas.Add(aux);
+                    }
+                }
+            }
+            else
+            {
+                if (CartaUno.Tipo == Carta.TipoCarta.Especial)
+                {
+                    this.JugadorDos.Cartas.RemoveAt(0);
+                    this.JugadorDos.Cartas.Add(CartaDos);
+                    this.JugadorUno.Cartas.OrderBy(x => x.Atributos.First());
+                }
+                if (CartaDos.Tipo == Carta.TipoCarta.Especial)
+                {
+                    this.JugadorUno.Cartas.RemoveAt(0);
+                    this.JugadorUno.Cartas.Add(CartaUno);
+                    this.JugadorDos.Cartas.OrderBy(x => x.Atributos.First());
+                }
+            }
+
+        }
 
         public void Mezclar()
         {
