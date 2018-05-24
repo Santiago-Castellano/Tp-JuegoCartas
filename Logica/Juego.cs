fbc1ae6e50ed3c48d54e491018fc86dfcba87c86
SantiagoCastellano <santiagocastellano01@gmail.com>
2018-05-24 13:22:32 -0300

    Agrega lógica para leer mazo de memoria

--- Logica/Juego.cs
+++ Logica/Juego.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,9 +15,45 @@ namespace Juego.Entidades
         public Juego()
         {
             this.Partidas = new List<Partida>();
-            this.Mazos = new List<Mazo>();
+            var lines = File.ReadAllLines(@"C:\Users\santi\Desktop\Tp-JuegoCartas");
+            Mazo mazo = new Mazo() {Nombre = lines[0] };
+            int contador = 0;
+            char[] delimitador = { '|' };
+            var forma = lines[1].Split(delimitador);
+            foreach (var line in lines)
+            {
+               if(contador > 1)
+                {
+                    var aux = line.Split(delimitador);
+                    Carta carta = new Carta()
+                    {
+                        Nombre =aux[1],
+                        Codigo = aux[0],
+                        Tipo = Carta.TipoCarta.Normal
+                    };
+                    this.AgregaAtributo(carta, aux, forma);
+                    mazo.Cartas.Add(carta);
+                }
+                contador += 1;
+            }
+            Mazos.Add(mazo);
         }
 
+        private void AgregaAtributo(Carta carta, string[] linea, string[] formadeatributos)
+        {
+            for (int i = 0; i < linea.Count(); i++)
+            {
+                if (i > 1)
+                {
+                    Atributo atributo = new Atributo()
+                    {
+                        Nombre = formadeatributos[i],
+                        Valor = Convert.ToDecimal(linea[i]),
+                    };
+                    carta.Atributos.Add(atributo);
+                }
+            }
+        }
 
         public List<Partida> ObtenerPartidasPendientes()
         {
