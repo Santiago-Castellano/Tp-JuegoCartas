f67c32acf5f8ba84ac0b2ff5c4be7eb9d8be8c3d
SantiagoCastellano <santiagocastellano01@gmail.com>
2018-06-09 01:39:45 -0300

    Agrega metodos al Juego

--- Logica/Juego.cs
+++ Logica/Juego.cs
@@ -15,28 +15,67 @@ namespace Juego.Entidades
         public Juego()
         {
             this.Partidas = new List<Partida>();
-            //var lines = File.ReadAllLines(@"C:\Users\santi\Desktop\Tp-JuegoCartas");
-            //Mazo mazo = new Mazo() {Nombre = lines[0] };
-            //int contador = 0;
-            //char[] delimitador = { '|' };
-            //var forma = lines[1].Split(delimitador);
-            //foreach (var line in lines)
-            //{
-            //   if(contador > 1)
-            //    {
-            //        var aux = line.Split(delimitador);
-            //        Carta carta = new Carta()
-            //        {
-            //            Nombre =aux[1],
-            //            Codigo = aux[0],
-            //            Tipo = Carta.TipoCarta.Normal
-            //        };
-            //        this.AgregaAtributo(carta, aux, forma);
-            //        mazo.Cartas.Add(carta);
-            //    }
-            //    contador += 1;
-            //}
-            //Mazos.Add(mazo);
+            this.Mazos = new List<Mazo>();
+            this.LeerMazos();
+            
+        }
+
+        private void LeerMazos()
+        {
+           var deckFolder = Directory.GetDirectories(@"C:\Users\santi\Desktop\Tp-JuegoCartas\Mazos");
+           foreach (var deck in deckFolder)
+           {
+                Mazo mazo = new Mazo();
+                
+                var lines = File.ReadAllLines(deck + "\\Informacion.txt");
+                mazo.Nombre = lines[0];
+                int contador = 0;
+                char[] delimitador = { '|' };
+                var forma = lines[1].Split(delimitador);
+                foreach (var line in lines)
+                {
+                    if (contador > 1)
+                    {
+                        var aux = line.Split(delimitador);
+                        Carta carta = new Carta()
+                        {
+                            Nombre = aux[1],
+                            Codigo = aux[0],
+                            Tipo = Carta.TipoCarta.Normal
+                        };
+                        this.AgregaAtributo(carta, aux, forma);
+                        mazo.Cartas.Add(carta);
+                    }
+                    contador += 1;
+                }
+                Mazos.Add(mazo);
+            }
+        }
+
+        public Partida UnirsePartida(string nombrejugador, string coneccionjugador,string nombrepartida)
+        {
+            Jugador jugadornuevo = new Jugador() {Nombre = nombrejugador, ConecctionID = coneccionjugador };
+            var partida = this.Partidas.SingleOrDefault(x => x.Nombre == nombrepartida);
+            partida.JugadorDos = jugadornuevo;
+            partida.ComenzarJuego();
+            return partida;
+        }
+
+        public Partida CrearNuevaPartida(string jugadorunonombre,string jugadorunoconeccion, string nombrepartida, string nombremazo)
+        {
+            var mazoseleccionado = this.Mazos.SingleOrDefault(x => x.Nombre == nombremazo);
+            var NuevoJugador = new Jugador() {Nombre = jugadorunonombre, ConecctionID = jugadorunoconeccion };
+            Partida partida = new Partida(mazoseleccionado, NuevoJugador) {Nombre = nombrepartida };
+            partida.JugadorUno = NuevoJugador;
+            Partidas.Add(partida);
+            return partida;
+        }
+
+        public void EliminarPartida(string nombrepartida)
+        {
+
+            var partidaeliminar = this.Partidas.SingleOrDefault(x => x.Nombre == nombrepartida);
+            this.Partidas.Remove(partidaeliminar);
         }
 
         private void AgregaAtributo(Carta carta, string[] linea, string[] formadeatributos)
